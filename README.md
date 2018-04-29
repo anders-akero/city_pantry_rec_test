bc562d60a48d1b45c48c30786d6e909da8d48954
Anders Åkerö <anders@akero.eu>
2018-04-29 22:09:34 +0100

    Create README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple filereader
+
+## Assumptions:
+ * Advance time is always given in hours
+
+## PHPUnit
+`$ ./vendor/bin/phpunit --bootstrap vendor/autoload.php tests --testdox`
--- cli/index.php
+++ cli/index.php
@@ -7,33 +7,4 @@ unset($inputs[0]);
 
 $data = new Input(array_values($inputs));
 
-// The following line can be used for debug purpose.
-// It changes the current time for the system.
-$data->setCurrentTime(New DateTime('2015-10-20 00:00'));//TODO: Remove this line to run in "real" time
 echo $data->getSuggestions();
-
-/**
- * Assumptions:
- * 1. Advance time is always given in hours
- * 2. That the output should not contain the advance time needed
- */
-
-/**
- * To run the tests:
- * `./vendor/bin/phpunit --bootstrap vendor/autoload.php tests --testdox`
- * The test assume that we have a file called "vendors.txt" in the data-foler.
- * The test also assume that this file contains the following:
- * "Grain and Leaf;E32NY;100
-    Grain salad;nuts;12h
-
-    Wholegrains;SW34DA;20
-    The Classic;gluten;24h
-
-    Ghana Kitchen;NW42QA;40
-    Premium meat selection;;36h
-    Breakfast;gluten,eggs;12h
-
-    Well Kneaded;EC32BA;150
-    Full English breakfast;gluten;24h
-"
- */
